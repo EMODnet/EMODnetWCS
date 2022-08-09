f97ae5160b3da3957cfd914d4143eb2316865493
Anna Krystalli <annakrystalli@googlemail.com>
2022-08-09 13:21:40 +0300

    change default version, add logger, move and add checks

---
+++ R/checks.R
@@ -0,0 +1,92 @@
+
+check_service_name <- function(service) {
+    checkmate::assert_choice(service, emodnet_wcs()$service_name)
+}
+
+check_wcs <- function(wcs) {
+    checkmate::assertR6(
+        wcs,
+        classes = c("WCSClient", "OWSClient", "OGCAbstractObject", "R6")
+    )
+}
+
+check_wcs_version <- function(wcs) {
+    supported_versions <- wcs$getCapabilities()$
+        getServiceIdentification()$
+        getServiceTypeVersion()
+
+    version <- wcs$getVersion()
+
+    if (!checkmate::test_choice(version, supported_versions)) {
+        rlang::warn(glue::glue('Service version {usethis::ui_value(version)} not',
+                               ' supported by server and can result in unexpected',
+                               ' behaviour.
+                            We strongly recommend reconnecting using one of the ',
+                               'supported versions: ',
+                               '{usethis::ui_value(supported_versions)}'))
+    }
+}
+
+# Checks if there is internet and performs an HTTP GET request
+perform_http_request <- function(service_url){
+    usethis::ui_oops("WCS client creation failed.")
+    usethis::ui_info("Service: {usethis::ui_value(service_url)}")
+
+    has_internet <- function() {
+        if (nzchar(Sys.getenv("NO_INTERNET_TEST_EMODNET"))) {
+            return(FALSE)
+        }
+        curl::has_internet()
+    }
+
+    if (!has_internet()) {
+        usethis::ui_info("Reason: There is no internet connection")
+        return(NULL)
+    }
+
+    service_url |>
+        paste0("?request=GetCapabilities") |>
+        httr::GET()
+
+}
+
+# Checks if there is internet connection and HTTP status of the service
+check_service <- function(request) {
+
+    if (is.null(request)) {
+        rlang::abort("WCS client creation failed.")
+    }
+
+    if (httr::http_error(request)) {
+        usethis::ui_info("HTTP Status: {crayon::red(httr::http_status(request)$message)}")
+        usethis::ui_line()
+
+        is_monitor_up <- !is.null(curl::nslookup("monitor.emodnet.eu", error = FALSE))
+        if (interactive() && is_monitor_up) {
+            if (usethis::ui_yeah("Browse the EMODnet OGC monitor?")) {
+                utils::browseURL("https://monitor.emodnet.eu/resources?lang=en&resource_type=OGC:WCS")
+            }
+        }
+
+        rlang::abort("Service creation failed")
+
+        # If no HTTP status, something else is wrong
+    } else if(!httr::http_error(request)) {
+        usethis::ui_info("HTTP Status: {crayon::green(httr::http_status(request)$message)}")
+        usethis::ui_stop("An exception has occurred. Please raise an issue in {packageDescription('EMODnetWFS')$BugReports}")
+    }
+
+}
+
+check_coverages <- function(wcs, coverages) {
+    checkmate::assert_character(coverages)
+    test_coverages <- coverages %in% get_cov_ids(wcs)
+
+    if (!all(test_coverages)) {
+        bad_coverages <- coverages[!test_coverages]
+        rlang::abort(
+            glue::glue("{usethis::ui_value(paste(bad_coverages, collapse = ', '))}",
+                       " not valid {switch(length(bad_coverages),'1' = 'coverage', 'coverages')}",
+                       " for service {usethis::ui_path(wcs$getUrl())}"))
+    }
+}
--- R/client.R
+++ R/client.R
@@ -2,7 +2,8 @@
 #'
 #' @param service the EMODnet OGC WCS service name.
 #' For available services, see [`emodnet_wcs()`].
-#' @param service_version the WCS service version. Defaults to "2.0.0".
+#' @param service_version the WCS service version. Defaults to "2.0.1".
+#' @param logger character string. Level of logger: 'NONE' for no logger, 'INFO' to get ows4R logs, 'DEBUG' for all internal logs (such as as Curl details)
 #'
 #' @return An [`ows4R::WCSClient`] R6 object with methods for interfacing an OGC Web Feature Service.
 #' @export
@@ -12,12 +13,17 @@
 #' \dontrun{
 #' wcs <- emodnet_init_wcs_client(service = "bathymetry")
 #' }
-emodnet_init_wcs_client <- function(service, service_version = c("2.1.0", "2.0.1", "1.1.1", "1.1.0", "1.0")) {
+emodnet_init_wcs_client <- function(service, service_version = c("2.0.1", "2.1.0", "2.0.0",
+                                                                 "1.1.1", "1.1.0",
+                                                                 "1.0"),
+                                    logger = c("NONE", "INFO", "DEBUG")) {
 
     check_service_name(service)
     service_version <- match.arg(service_version)
-
+    logger <- match.arg(logger)
+    if (logger == "NONE") logger <- NULL
     service_url <- get_service_url(service)
+
 
     create_client <- function(){
 
@@ -35,13 +41,17 @@ emodnet_init_wcs_client <- function(service, service_version = c("2.1.0", "2.0.1
             service_url,
             serviceVersion = service_version,
             headers = c("User-Agent" = "EMODnetWCS R package https://github.com/EMODnet/EMODnetWCS"),
-            config = config
+            config = config,
+            logger = logger
         ))
 
         check_wcs(wcs)
+
         usethis::ui_done("WCS client created succesfully")
         usethis::ui_info("Service: {usethis::ui_value(wcs$getUrl())}")
         usethis::ui_info("Version: {usethis::ui_value(wcs$getVersion())}")
+
+        check_wcs_version(wcs)
 
         wcs
     }
@@ -50,76 +60,6 @@ emodnet_init_wcs_client <- function(service, service_version = c("2.1.0", "2.0.1
         create_client(),
         error = function(e) {check_service(perform_http_request(service_url))}
     )
-
-}
-
-check_service_name <- function(service) {
-    checkmate::assert_choice(service, emodnet_wcs()$service_name)
-}
-
-check_wcs <- function(wcs) {
-    checkmate::assertR6(
-        wcs,
-        classes = c("WCSClient", "OWSClient", "OGCAbstractObject", "R6")
-    )
-}
-
-get_service_url <- function(service) {
-    emodnet_wcs()$service_url[emodnet_wcs()$service_name == service]
-}
-
-get_service_name <- function(service_url) {
-    emodnet_wcs()$service_name[emodnet_wcs()$service_url == service_url]
-}
-
-# Checks if there is internet and performs an HTTP GET request
-perform_http_request <- function(service_url){
-    usethis::ui_oops("WCS client creation failed.")
-    usethis::ui_info("Service: {usethis::ui_value(service_url)}")
-
-    has_internet <- function() {
-        if (nzchar(Sys.getenv("NO_INTERNET_TEST_EMODNET"))) {
-            return(FALSE)
-        }
-        curl::has_internet()
-    }
-
-    if (!has_internet()) {
-        usethis::ui_info("Reason: There is no internet connection")
-        return(NULL)
-    }
-
-    service_url |>
-        paste0("?request=GetCapabilities") |>
-        httr::GET()
-
-}
-
-# Checks if there is internet connection and HTTP status of the service
-check_service <- function(request) {
-
-    if (is.null(request)) {
-        rlang::abort("WCS client creation failed.")
-    }
-
-    if (httr::http_error(request)) {
-        usethis::ui_info("HTTP Status: {crayon::red(httr::http_status(request)$message)}")
-        usethis::ui_line()
-
-        is_monitor_up <- !is.null(curl::nslookup("monitor.emodnet.eu", error = FALSE))
-        if (interactive() && is_monitor_up) {
-            if (usethis::ui_yeah("Browse the EMODnet OGC monitor?")) {
-                utils::browseURL("https://monitor.emodnet.eu/resources?lang=en&resource_type=OGC:WCS")
-            }
-        }
-
-        rlang::abort("Service creation failed")
-
-        # If no HTTP status, something else is wrong
-    } else if(!httr::http_error(request)) {
-        usethis::ui_info("HTTP Status: {crayon::green(httr::http_status(request)$message)}")
-        usethis::ui_stop("An exception has occurred. Please raise an issue in {packageDescription('EMODnetWFS')$BugReports}")
-    }
 
 }
 
