c67e8816ea1e99b19779d34f316a61922621e880
Anna Krystalli <annakrystalli@googlemail.com>
2022-08-17 18:28:54 +0300

    correct bbox, constraints, dims & validate namespaces

--- R/utils.R
+++ R/utils.R
@@ -25,7 +25,16 @@ get_cov_ids <- function(wcs) {
 }
 
 get_bbox <- function(summary) {
-    summary$getBoundingBox()$BoundingBox$getBBOX()
+    #summary$getBoundingBox()$BoundingBox$getBBOX()
+    boundaries <- summary$getDescription()$boundedBy
+    upper <- unlist(c(boundaries$upperCorner))
+    lower <- unlist(c(boundaries$lowerCorner))
+
+    sf::st_bbox(c(xmin = lower[1],
+                xmax = upper[1],
+                ymin = lower[2],
+                ymax = upper[2]),
+                crs = extr_bbox_crs(summary))
 }
 
 conc_bbox <- function(bbox) {
@@ -33,7 +42,21 @@ conc_bbox <- function(bbox) {
 }
 
 extr_bbox_crs <- function(summary) {
-    summary$getBoundingBox()$BoundingBox$attrs$crs
+
+    bbox_crs <- summary$getBoundingBox()$BoundingBox$attrs$crs
+
+    if(!is.null(bbox_crs)){
+        crs_parts <- unlist(strsplit(bbox_crs, "EPSG:"))
+        if(length(crs_parts)==2){
+            srid <- as.integer(crs_parts[2])
+            if(!is.na(srid)) bbox_crs <- sf::st_crs(srid)
+        } else {
+            bbox_crs <- sf::st_crs(4326)
+        }
+    } else {
+        bbox_crs <- sf::st_crs(4326)
+    }
+    return(bbox_crs)
 }
 
 get_WGS84bbox <- function(summary) {
@@ -53,8 +76,54 @@ get_uom <- function(summary) {
 }
 
 get_constraint <- function(summary) {
-    summary$getDescription()$rangeType$DataRecord$field$Quantity$constraint$AllowedValues$interval$value
+    summary$getDescription()$rangeType$DataRecord$field$Quantity$constraint$
+        AllowedValues$interval$value |> strsplit(" ") |> unlist() |>
+        as.numeric() |> paste(collapse = ", ")
+
 }
+
+
+get_grid_size <- function(summary, type = c("character", "numeric")) {
+    type <- match.arg(type)
+
+    grid_envelope <- summary$getDescription()$domainSet$limits$GridEnvelope
+    low <- grid_envelope$low$value |> strsplit(" ") |> unlist() |> as.numeric()
+    high <- grid_envelope$high$value |> strsplit(" ") |> unlist() |> as.numeric()
+    diff <- high - low  + 1
+
+    switch (type,
+            character = paste(diff, collapse = "x"),
+            numeric = diff
+    )
+
+}
+
+get_resolution <- function(summary, type = c("character", "numeric")) {
+    type <- match.arg(type)
+
+    boundaries <- summary$getDescription()$boundedBy
+    upper_crn <- boundaries$upperCorner[1,] |> unlist()
+    lower_crn <- boundaries$lowerCorner[1,]  |> unlist()
+    grid_size <- get_grid_size(summary, type = "numeric")
+    uom <- boundaries$attrs$uomLabels
+
+    resolution <- (upper_crn - lower_crn) / grid_size
+    attr(resolution, "uom") <- uom
+
+    switch (type,
+            character = paste(paste0(resolution,
+                                     unlist(strsplit(uom, " "))),
+                              collapse = " x "),
+            numeric = resolution
+    )
+
+}
+
+get_coverage_function <- function(summary, param = c("sequenceRule", "startPoint")) {
+    param <- match.arg(param)
+    summary$getDescription()$coverageFunction[[1]][[param]]$value
+}
+
 
 error_wrap <- function(expr) {
     tryCatch(expr, error = function(e) NA)
@@ -63,7 +132,6 @@ error_wrap <- function(expr) {
 process_dimension <- function(x, format = c("character", "list", "tibble")) {
     format <- match.arg(format)
     dimensions <- x$getDimensions()
-
 
     # internal format specific processing functions
     process_character <- function(x) {
@@ -95,4 +163,25 @@ process_dimension <- function(x, format = c("character", "list", "tibble")) {
            "character" = process_character(dimensions),
            "list" = process_list(dimensions),
            "tibble" = process_tibble(dimensions))
+}
+
+get_dimensions_names <- function(summary) {
+    dimensions <- summary$getDescription()$boundedBy$attrs
+
+    paste(
+        paste0(unlist(strsplit(dimensions$axisLabels, " ")),
+               " (",
+               unlist(strsplit(dimensions$uomLabels, " ")),
+               ")"),
+        collapse = ", ")
+}
+
+get_dimensions_n <- function(summary) {
+    summary$getDescription()$boundedBy$attrs$srsDimension |>
+        as.integer()
+
+}
+
+validate_namespace <- function(coverage) {
+    gsub(":", "__", coverage)
 }
