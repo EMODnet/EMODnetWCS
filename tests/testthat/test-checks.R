af550864afe900c61f546044c126af4a784fd410
Anna Krystalli <annakrystalli@googlemail.com>
2022-09-08 13:24:17 +0300

    test check_coverages

--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -32,9 +32,29 @@ test_that("validate_bbox works", {
 
 })
 
+test_that("check bbox works", {
+    expect_equal(error_wrap(stop()), NA)
+    expect_equal(error_wrap(NULL), NA)
+    expect_equal(error_wrap("success"), "success")
+})
+
+
 test_that("error wrap works", {
     expect_equal(error_wrap(stop()), NA)
     expect_equal(error_wrap(NULL), NA)
     expect_equal(error_wrap("success"), "success")
+})
+
+
+test_that("check coverages works", {
+    wcs <- create_biology_wcs()
+    coverage_ids <- c(
+        "Emodnetbio__ratio_large_to_small_19582016_L1_err",
+        "Emodnetbio__aca_spp_19582016_L1",
+        "Emodnetbio__cal_fin_19582016_L1_err",
+        "Emodnetbio__cal_hel_19582016_L1_err"
+    )
+    expect_invisible(check_coverages(wcs, coverage_ids))
+    expect_error(check_coverages(wcs, "erroneous_id"))
 })
 
